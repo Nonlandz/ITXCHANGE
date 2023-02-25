f0637952b896707586dd43888a01b45aa543e3d5
Nonlandz <88500373+Nonlandz@users.noreply.github.com>
2023-02-26 00:17:49 +0700

    Update Signin.vue

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="flex justify-content-center bg-primary w-full">
-sad
-  </div>
+ <div class="container">
+	<div class="heading">Login</div>
+	<form>
+		<input type="email" placeholder="Email or Phone" class="input">
+		<input type="password" placeholder="Password" class="input">
+		<a href="#" class="forgot-password">Forgot Password?</a>
+		<input type="submit" value="Log In" class="button">
+	</form>
+	<a href="#" class="create-account">Create New Account</a>
+</div>
 </template>
