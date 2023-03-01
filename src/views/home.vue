d2720fa3f9136bae6e0a2aee4afac174389b4136
24152415 <88503457+24152415@users.noreply.github.com>
2023-03-02 01:05:04 +0700

    up home

--- src/views/home.vue
+++ src/views/home.vue
@@ -6,18 +6,18 @@
 
   <body>
    <div class="justify-content-center text-center " >
-   <Button label="Headphone" class="p-button-rounded p-button-secondary mr-5 mb-4" />
-   <Button label="Mouse" class="p-button-rounded p-button-secondary mr-5 mb-4" />
-   <Button label="Keyboard" class="p-button-rounded p-button-secondary mr-5 mb-4" />
-   <Button label="Monitor" class="p-button-rounded p-button-secondary mr-5 mb-4" />
-   <Button label="Printer" class="p-button-rounded p-button-secondary mr-5 mb-4" />
-   <Button label="Smartphone" class="p-button-rounded p-button-secondary mr-5 mb-4" />
-   <Button label="Ipad" class="p-button-rounded p-button-secondary mr-5 mb-4" />
-   <Button label="Notebook" class="p-button-rounded p-button-secondary mr-5 mb-4" />
-   <Button label="Other" class="p-button-rounded p-button-secondary mr-5 mb-4" />
+   <Button label="Headphone" class="p-button-rounded bg-bluegray-200 text-black hover:bg-bluegray-400  mr-5 mb-4" />
+   <Button label="Mouse" class="p-button-rounded bg-bluegray-200 text-black hover:bg-bluegray-400 mr-5 mb-4" />
+   <Button label="Keyboard" class="p-button-rounded bg-bluegray-200 text-black hover:bg-bluegray-400 mr-5 mb-4" />
+   <Button label="Monitor" class="p-button-rounded bg-bluegray-200 text-black hover:bg-bluegray-400 mr-5 mb-4" />
+   <Button label="Printer" class="p-button-rounded bg-bluegray-200 text-black hover:bg-bluegray-400 mr-5 mb-4" />
+   <Button label="Smartphone" class="p-button-rounded bg-bluegray-200 text-black hover:bg-bluegray-400 mr-5 mb-4" />
+   <Button label="Ipad" class="p-button-rounded bg-bluegray-200 text-black hover:bg-bluegray-400 mr-5 mb-4" />
+   <Button label="Notebook" class="p-button-rounded bg-bluegray-200 text-black hover:bg-bluegray-400 mr-5 mb-4" />
+   <Button label="Other" class="p-button-rounded bg-bluegray-200 text-black hover:bg-bluegray-400 mr-5 mb-4" />
   </div>
-  <div class="w-10 h-6rem bg-secondary border-round-xl text-center mt-4" style="margin-left: 7.5%;">
-    <Button label="add post" class="p-button-rounded p-button-secondary mt-4" />
+  <div class="w-10 h-6rem bg-bluegray-200 text-black border-round-xl text-center mt-4" style="margin-left: 7.5%;">
+    <Button label="add post" class="p-button-rounded bg-primary-200 text-black hover:bg-primary-300 mt-4" />
   </div>
   
 
