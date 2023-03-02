72f4736b656df692f14961d943f8bf06dde1788c
Nonlandz <88500373+Nonlandz@users.noreply.github.com>
2023-03-03 01:20:42 +0700

    Update login.vue

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,55 +1,60 @@
 <template lang="">
-<div class="body">
-  <div class="container">
-    <input type="checkbox" id="flip">
+  <div class="body">
+    <div class="container">
+      <input type="checkbox" id="flip" />
       <div class="cover">
-     <div class="front">
-       <img src="https://cdn.discordapp.com/attachments/883221363316375583/1079743566240301097/blank-profile-picture-973460__340.png" alt="">
-       <div class="text">
-         <span class="text-1 text-dark">ITxChange<br> </span>
-         <span class="text-2 text-dark">Let's Change Something New! </span>
-       </div>
-     </div>
-     <div class="back">
-       <!--<img class="backImg" src="images/backImg.jpg" alt="">-->
-       <div class="text">
-         <span class="text-1">Complete miles of journey <br> with one step</span>
-         <span class="text-2">Let's get started</span>
-       </div>
-     </div>
-   </div>
-   <div class="forms">
-       <div class="form-content">
-         <div class="login-form">
-           <div class="title">Login</div>
-         <form action="#">
-           <div class="input-boxes">
-             <div class="input-box">
-               <i class="pi pi-envelope"></i>
-               <input type="text" v-model="username" placeholder="Enter your username">
-             </div>
-             <div class="input-box">
-               <i class="fas fa-lock"></i>
-               <input type="password" v-model="password" placeholder="Enter your password">
-             </div>
-             <router-link to="/forgotpass">
-             <div class="text"><a href="#">Forgot password?</a></div>
-            </router-link>
-             <div class="button input-box" >
-               <input type="submit" value="Submit" @click="signin()">
-             </div>
-             <div class="text sign-up-text">Don't have an account?
-               <router-link to="/signup"><label for="flip">Signup now</label>
-              </router-link> </div>
-               
-           </div>
-       </form>
-     
-   </div>
-   </div>
-   </div>
- </div>
-</div>
+        <div class="front">
+          <img
+            src="https://cdn.discordapp.com/attachments/883221363316375583/1079743566240301097/blank-profile-picture-973460__340.png"
+            alt=""
+          />
+          <div class="text">
+            <span class="text-1 text-dark">ITxChange<br /> </span>
+            <span class="text-2 text-dark">Let's Change Something New! </span>
+          </div>
+        </div>
+        <div class="back">
+          <!--<img class="backImg" src="images/backImg.jpg" alt="">-->
+          <div class="text">
+            <span class="text-1"
+              >Complete miles of journey <br />
+              with one step</span
+            >
+            <span class="text-2">Let's get started</span>
+          </div>
+        </div>
+      </div>
+      <div class="forms">
+        <div class="form-content">
+          <div class="login-form">
+            <div class="title">Login</div>
+            <form action="#">
+              <div class="input-boxes">
+                <div class="input-box">
+                  <i class="pi pi-envelope"></i>
+                  <input type="text" v-model="username" placeholder="Enter your username" />
+                </div>
+                <div class="input-box">
+                  <i class="fas fa-lock"></i>
+                  <input type="password" v-model="password" placeholder="Enter your password" />
+                </div>
+                <router-link to="/forgotpass">
+                  <div class="text"><a href="#">Forgot password?</a></div>
+                </router-link>
+                <div class="button input-box">
+                  <input type="submit" value="Submit" @click="signin()" />
+                </div>
+                <div class="text sign-up-text">
+                  Don't have an account?
+                  <router-link to="/signup"><label for="flip">Signup now</label> </router-link>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <!-- <script src="./data.js"></script>
@@ -73,20 +78,18 @@
 export default {
   data() {
     return {
-      username: "",
-      password: "",
+      username: '',
+      password: '',
       user: []
     }
   },
   mounted() {
-    var retrievedObject = JSON.parse(localStorage.getItem('user'));
-    this.user = retrievedObject;
-
+    var retrievedObject = JSON.parse(localStorage.getItem('user'))
+    this.user = retrievedObject
   },
   methods: {
-
     signin: function () {
-      console.log("sa");
+      console.log('sa')
 
       // if (this.username != "user.username") {
       //   alert("asdsad")
@@ -94,24 +97,18 @@ export default {
       // if (this.password == "user.password") {
       //   alert("asdsad")
       // }
-
     }
   }
 }
 </script>
 
-
-
-
-
-
-<style  scoped>
+<style scoped>
 .body {
   min-height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
-  background: #1E4F79;
+  background: #1e4f79;
   padding: 30px;
 }
 
@@ -137,7 +134,7 @@ export default {
   transform-style: preserve-3d;
 }
 
-.container #flip:checked~.cover {
+.container #flip:checked ~ .cover {
   transform: rotateY(-180deg);
 }
 
@@ -235,7 +232,7 @@ export default {
   bottom: 0;
   height: 3px;
   width: 25px;
-  background: #EB6648;
+  background: #eb6648;
 }
 
 .forms .signup-form .title:before {
@@ -274,14 +271,14 @@ export default {
 
 .form-content .input-box i {
   position: absolute;
-  color: #EB6648;
+  color: #eb6648;
   font-size: 17px;
 }
 
 .forms .form-content .text {
   font-size: 14px;
   font-weight: 500;
-  color: #1E4F79;
+  color: #1e4f79;
 }
 
 .forms .form-content .text a {
@@ -299,7 +296,7 @@ export default {
 
 .forms .form-content .button input {
   color: #fff;
-  background: #EB6648;
+  background: #eb6648;
   border-radius: 6px;
   padding: 0;
   cursor: pointer;
@@ -311,7 +308,7 @@ export default {
 }
 
 .forms .form-content label {
-  color: #EB6648;
+  color: #eb6648;
   cursor: pointer;
 }
 
@@ -343,13 +340,12 @@ export default {
     display: none;
   }
 
-  .container #flip:checked~.forms .signup-form {
+  .container #flip:checked ~ .forms .signup-form {
     display: block;
   }
 
-  .container #flip:checked~.forms .login-form {
+  .container #flip:checked ~ .forms .login-form {
     display: none;
   }
-
 }
 </style>
