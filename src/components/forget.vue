09b3c4b0db6395601344000f72c641909d843c73
Nonlandz <88500373+Nonlandz@users.noreply.github.com>
2023-03-03 10:58:41 +0700

    update

---
+++ src/components/forget.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="container">
+      <div class="form-wrap">
+        <h2>Forget Password</h2>
+        <form>
+          <div class="form-group">
+            <label for="username">Username:</label>
+            <input type="username" class="form-control" id="username" v-model="username">
+          </div>
+          <button type="submit" class="btn btn-primary btn-block" @click.prevent="submit">{{ buttonLabel }}</button>
+        </form>
+        <p class="text-center mt-3">{{ message }}</p>
+        <p class="text-center mt-3">
+          <i class="fas fa-arrow-left mr-1"></i>
+          <router-link to="/">
+          <a >Back to login</a>
+        </router-link>
+        </p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        isSubmitted: false
+      }
+    },
+    computed: {
+      buttonLabel() {
+        return this.isSubmitted ? 'Password changed!' : 'Submit';
+      },
+      message() {
+        return this.isSubmitted ? 'Check your username for further instructions.' : '';
+      }
+    },
+    methods: {
+      submit() {
+        // Handle form submission
+        this.isSubmitted = true;
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  body {
+    background-color: #f8f9fa;
+    font-family: 'Roboto', sans-serif;
+  }
+  
+  .container {
+    max-width: 500px;
+    margin: 0 auto;
+    padding-top: 100px;
+  }
+  
+  .form-wrap {
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+    padding: 30px;
+  }
+  
+  .form-wrap h2 {
+    font-size: 28px;
+    font-weight: 700;
+    margin-top: 0;
+    margin-bottom: 30px;
+    text-align: center;
+  }
+  
+  .form-group label {
+    font-size: 16px;
+    font-weight: 700;
+    margin-bottom: 10px;
+    display: block;
+  }
+  
+  .form-control {
+    font-size: 16px;
+    border-radius: 5px;
+    padding: 10px 15px;
+    border: none;
+    box-shadow: none;
+    background-color: #f8f9fa;
+    border: 1px solid #ced4da;
+  }
+  
+  .form-control:focus {
+    box-shadow: none;
+    border-color: #80bdff;
+  }
+  
+  .btn-primary {
+    background-color: #007bff;
+    border: none;
+    box-shadow: none;
+    border-radius: 5px;
+    font-size: 16px;
+    font-weight: 700;
+    padding: 10px 15px;
+  }
+  
+  .btn-primary:hover {
+    background-color: #0069d9;
+  }
+  
+  .text-center {
+    text-align: center;
+  }
+  </style>
+  
--- src/views/forgotpass.vue
+++ src/views/forgotpass.vue
@@ -1,6 +1,62 @@
 <template>
-    <div>
-      forgotpass
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 mx-auto">
+        <div class="card mt-5">
+          <div class="card-header">
+            <h3 class="text-center">Reset Password</h3>
+          </div>
+          <div class="card-body">
+            <form @submit.prevent="resetPassword">
+              <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" v-model="username" required>
+              </div>
+              <div class="form-group">
+                <label for="newPassword">New Password</label>
+                <input type="password" class="form-control" id="newPassword" v-model="newPassword" required>
+              </div>
+              <div class="form-group">
+                <label for="confirmPassword">Confirm Password</label>
+                <input type="password" class="form-control" id="confirmPassword" v-model="confirmPassword" required>
+              </div>
+              <div class="text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+            <p class="mt-3 text-center">{{ message }}</p>
+          </div>
+        </div>
+      </div>
     </div>
-  </template>
-  
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      newPassword: '',
+      confirmPassword: '',
+      message: ''
+    }
+  },
+  methods: {
+    resetPassword() {
+      const storedUsername = localStorage.getItem('username');
+      if (this.username === storedUsername) {
+        if (this.newPassword === this.confirmPassword) {
+          localStorage.setItem('password', this.newPassword);
+          this.message = 'Password changed successfully!';
+        } else {
+          this.message = 'New password and confirm password do not match.';
+        }
+      } else {
+        this.message = 'Username does not match.';
+      }
+    }
+  }
+}
+</script>
